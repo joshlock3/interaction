f71ed37b3f1af37e2cb99e1c2ee8a53d6be146d7
Joshua Lockhart <josh@objectuve.com>
2018-07-03 13:42:37 -0500

    WIP

--- lib/interaction/input_validator.rb
+++ lib/interaction/input_validator.rb
@@ -13,21 +13,26 @@ module Interaction
       def require_input(*expected_inputs)
         self.class_eval do
           @expected_inputs = expected_inputs
-          original_method = instance_method(:call)
+          # original_method = self.instance_method(:call)
 
-          define_method(:call) do
-            required_inputs = self.class.expected_inputs
-
-            inputs_not_given = required_inputs - @input.to_h.keys
-            raise InputError, "#{inputs_not_given} missing from inputs" unless inputs_not_given.empty?
-
-            inputs_with_nil_values = required_inputs.select do |i|
-              @input.to_h[i].nil? || @input.to_h[i].size.zero?
-            end
-            raise InputError, "#{inputs_with_nil_values} was given blank input" unless inputs_with_nil_values.empty?
-
-            original_method.bind(self).call
+          def call
+            super
+            puts "I was here"
           end
+
+          # define_method(:call) do
+          #   required_inputs = self.class.expected_inputs
+          #
+          #   inputs_not_given = required_inputs - @input.to_h.keys
+          #   raise InputError, "#{inputs_not_given} missing from inputs" unless inputs_not_given.empty?
+          #
+          #   inputs_with_nil_values = required_inputs.select do |i|
+          #     @input.to_h[i].nil? || @input.to_h[i].size.zero?
+          #   end
+          #   raise InputError, "#{inputs_with_nil_values} was given blank input" unless inputs_with_nil_values.empty?
+          #
+          #   original_method.bind(self).call
+          # end
         end
 
         # self.class_eval do
