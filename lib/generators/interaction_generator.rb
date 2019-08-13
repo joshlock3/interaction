b77b50e13b737c297641ca5cdd7812a414ae1b41
Joshua Lockhart <josh@objectuve.com>
2019-08-13 12:15:09 -0500

    added spec generator

--- lib/generators/interaction_generator.rb
+++ lib/generators/interaction_generator.rb
@@ -8,13 +8,16 @@ module Interaction
     class InteractionGenerator < Rails::Generators::NamedBase
       include Rails::Generators::ResourceHelpers
       source_root File.expand_path('../templates', __FILE__)
-      argument :interaction_name, type: :string
-      class_option :inputs, type: :array, default: [], desc: "List of inputs to the interaction"
+      class_option :inputs, type: :array, aliases: "-i", default: [], desc: "List of inputs to the interaction"
 
       desc "Generates a Interaction (Service Object) with the given NAME"
 
       def copy_initializer
-        template "interaction.template", "app/interactions/#{interaction_name}.rb"
+        template "interaction.template", "app/interactions/#{name}.rb"
+      end
+
+      def copy_spects
+        template "interaction_spec.template", "spec/interactions/#{name}_spec.rb"
       end
     end
   end
