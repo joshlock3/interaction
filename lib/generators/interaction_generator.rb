46973aea6d3853d6febfc5aee9367a14bb6dc53d
Joshua Lockhart <joshualockhart@ymail.com>
2019-08-13 12:16:19 -0500

    Generators
    * added generator
    
    * added spec generator

---
+++ lib/generators/interaction_generator.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails/generators/base'
+
+module Interaction
+  module Generators
+
+    class InteractionGenerator < Rails::Generators::NamedBase
+      include Rails::Generators::ResourceHelpers
+      source_root File.expand_path('../templates', __FILE__)
+      class_option :inputs, type: :array, aliases: "-i", default: [], desc: "List of inputs to the interaction"
+
+      desc "Generates a Interaction (Service Object) with the given NAME"
+
+      def copy_initializer
+        template "interaction.template", "app/interactions/#{name}.rb"
+      end
+
+      def copy_spects
+        template "interaction_spec.template", "spec/interactions/#{name}_spec.rb"
+      end
+    end
+  end
+end
