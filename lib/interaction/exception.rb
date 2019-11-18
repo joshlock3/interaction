9650d3b5f2024b8b5245238ecae33a838a8d2652
Joshua Lockhart <josh@objectuve.com>
2019-11-18 07:58:55 -0600

    added exception message

--- lib/interaction/exception.rb
+++ lib/interaction/exception.rb
@@ -1,7 +1,7 @@
 module Interaction
   module Exception
     def self.report(error: nil, tags: nil, class_name: nil)
-      error = %[[#{class_name} Error] #{error}]
+      error = %[[#{class_name} Error] #{error}\n #{error.message}]
       Raven.capture_exception(error, tags: tags) if defined? Raven
       Rails.logger.error error if defined? Rails
     end
